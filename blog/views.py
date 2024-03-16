9862cd1bc8f590946e39cb3cd3b4321db65406aa
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-03-16 17:51:02 +0200

    set up pagination, tried fixing the search query but not yet working,created a new about.html

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from category.models import Category
 from .models import Post
 from django.http import Http404
-# from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
 
 def home(request, category_slug=None):
@@ -23,29 +24,52 @@ def home(request, category_slug=None):
 
     featured = Post.articlemanager.filter(featured=True)[0:4]
 
+    # get query from request
+    query = request.GET.get("query")
+    # Set query to '' if None
+    if query is None:
+        query = ""
+    # search for query in headline, sub headline, body
+    articles = Post.articlemanager.filter(
+        Q(title__icontains=query)
+        | Q(sub_title__icontains=query)
+        | Q(content__icontains=query)
+    )
+
     context = {
         "posts": posts,
         "featured": featured,
         "category": category,
+        "articles": articles,
     }
 
     return render(request, "blog/home.html", context)
 
 
 def posts(request):
-
-    articles = Post.articlemanager.filter(featured=True)[0:4]
+    ####### pagination#####
+    articles = Post.articlemanager.filter(status="published")[0:4]
+    p = Paginator(articles, 4)  # creating a paginator object
+    # getting the desired page number from url
+    page_number = request.GET.get("page")
+    try:
+        page_obj = p.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = p.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = p.page(p.num_pages)
 
     context = {
         "articles": articles,
+        "page_obj": page_obj,
     }
 
     return render(request, "posts/posts.html", context)
 
 
-def single_post(request, post):  # post
-
-    # post = get_object_or_404(Post, slug=post, status="published")
+def single_post(request, post):
     try:
         post = get_object_or_404(Post, slug=post, status="published")
     except Post.DoesNotExist:
@@ -60,17 +84,3 @@ def about(request):
 
 def contact(request):
     return render(request, "contact/contact.html")
-
-
-# get query from request
-# query = request.GET.get("query")
-# Set query to '' if None
-# if query is None:
-#     query = ""
-
-# search for query in headline, sub headline, body
-# articles = Post.articlemanager.filter(
-#     Q(title__icontains=query)
-#     | Q(sub_title__icontains=query)
-#     | Q(content__icontains=query)
-# )
